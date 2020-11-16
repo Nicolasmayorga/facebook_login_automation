d501d6e9adb8cda8f89c2008cd8d48b02a44af79
Nicolasmayorga <nmayorgav@gmail.com>
2020-11-15 23:09:42 -0500

    Este es el commit final

--- Login_automatization.py
+++ Login_automatization.py
@@ -13,9 +13,9 @@ driver.delete_all_cookies()
 
 #navigate to the url
 driver.get("https://www.facebook.com")
-driver.find_element_by_xpath('//*[@id="email"]').send_keys("nmayorgav@gmail.com")
+driver.find_element_by_xpath('//*[@id="email"]').send_keys("") #Your mail between ("")
 time.sleep(1)
-driver.find_element_by_xpath('//*[@id="pass"]').send_keys("8C@racteres")
+driver.find_element_by_xpath('//*[@id="pass"]').send_keys("") ##Your password between ("")
 time.sleep(1)
 driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').send_keys(Keys.ENTER)
 time.sleep(1)
